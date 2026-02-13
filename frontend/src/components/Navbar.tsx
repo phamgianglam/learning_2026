9dc8fa035ccdf05dec2dee55a8a0eaaf4208eee5
lamgpham <phamgianglam1996@gmail.com>
2026-02-13 22:35:08 +0700

    update navbnar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,13 @@
 
 export default function Navbar () {
-    return <div class="nav-bar">
-        <button>Home</button>
-        <input type="text" placeholder="search"></input>
+    const navStyle = "nav h-15 bg-gray-400 fixed flex top-0 left-0 right-0 w-full items-center";
+    const buttonStyle = "";
+    const logoLocation: string = "../asset/logo.png" 
+    return (
+    <div className="nav h-15 bg-gray-400 fixed flex top-0 left-0 right-0 w-full items-center">
+        <a href="loginpage"><img src={logoLocation} alt="logo" className="size-10 m-0.5"/></a>
+        <button className="p-0.5 bg-gray-500 border-x-2 text-white ">Home</button>
+        <input className="ml-auto mr-20 w-[15%] bg-amber-50" type="text" placeholder="search"/>
     </div>
+    )
 }
