93e7781712a58558c3dd63c845c46626af3d3e29
lamgpham <phamgianglam1996@gmail.com>
2026-02-19 00:00:00 +0700

    modify taskbar holder

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,9 +2,9 @@
 export default function Navbar () {
     const navStyle: string = "flex h-20 w-full top-0 nav bg-gray-400 items-center";
     const mainNavContent: string = "flex h-[80%] w-250 mx-auto left-0 right-0 items-center justify-evenly"
-    const logoStyle = "h-[75%] aspect-square";
-    const buttonStyle = "h-[75%] hover:bg-gray-500 rounded-xl ml-2"
-    const inputSyle = "bg-white h-[75%] ml-auto mr-20 w-[50%]" 
+    const logoStyle = "h-[90%] aspect-square";
+    const buttonStyle = "h-[90%] hover:bg-gray-500 rounded-xl ml-2 aspect-square"
+    const inputSyle = "bg-white h-[90%] ml-auto mr-20 w-[50%] border rounded-xl" 
     const logoLocation: string = "../asset/logo.png" 
     return (
     <div className={navStyle}>
@@ -12,7 +12,6 @@ export default function Navbar () {
                 <a href="loginpage"><img src={logoLocation} alt="logo" className={logoStyle}/></a>
                 <button className={buttonStyle}>Home</button>
                 <input className={inputSyle} type="text" placeholder="search"/>
-                {/* <a href="" */}
         </div>
     </div>
     )
